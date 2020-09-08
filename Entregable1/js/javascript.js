8febfa313400804dd37722a07bb6cb324087af49
unknown <fede120400@gmail.com>
2020-09-08 02:27:27 -0300

    Primer pusheo, faltan arreglos

---
+++ Entregable1/js/javascript.js
@@ -0,0 +1,225 @@
+function loadPage() {
+    let canvas = document.querySelector('#js-paint');
+    let ctx = canvas.getContext('2d');
+    let img;
+    let escalaFoto;
+    ctx.lineCap = 'round';
+
+    //FUNCION SELECTOR DE COLOR
+    let colorPicker = document.querySelector('#js-color-picker');
+    function cambiarColor() {
+        let color = colorPicker.value;
+        if (document.querySelector('#dibujar').checked) {
+            ctx.globalCompositeOperation = 'source-over';
+            ctx.strokeStyle = color;
+        }
+        else if (document.querySelector('#borrar').checked) {
+            ctx.globalCompositeOperation = 'destination-out';
+        }
+    }
+
+    //FUNCION GROSOR PINCEL    
+    let lineWidthRange = document.querySelector('#js-line-range');
+    let lineWidthLabel = document.querySelector('#js-range-value');
+    function rangoPincel() {
+        let width = lineWidthRange.value;
+        lineWidthLabel.innerHTML = width + " Px";
+        ctx.lineWidth = width;
+    }
+
+    let x = 0, y = 0;
+    let isMouseDown = false;
+
+    function stopDrawing() {
+        isMouseDown = false;
+    }
+
+    function startDrawing() {
+        isMouseDown = true;
+        [x, y] = [event.offsetX, event.offsetY];
+    }
+
+    function drawLine() {
+        if (isMouseDown) {
+            let newX = event.offsetX;
+            let newY = event.offsetY;
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(newX, newY);
+            ctx.stroke();
+            x = newX;
+            y = newY;
+        }
+    }
+
+    let btnLimpiar = document.querySelector('#btn-limpiar');
+    function limpiar() {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+
+    //FUNCION GUARDAR FOTO
+    let saveButton = document.querySelector('#btn-guardar');
+    function guardar() {
+        //El prompt es parecido al alert pero te deja meter un valor
+        let imageName = prompt('Ingrese el nombre de su foto');
+        if (imageName.toLowerCase() != "") {
+            let canvasDataURL = canvas.toDataURL();
+            let a = document.createElement('a');
+            a.href = canvasDataURL;
+            a.download = imageName;
+            a.click();
+        }
+        else {
+            alert("Usted debe colocarle un nombre a la imagen para poder guardarla");
+        }
+    }
+
+    //FUNCION CARGAR FOTO
+    let input = document.querySelector('#inputCargar');
+    //Vacia el canvas
+    let context = canvas.getContext('2d');
+    context.fillStyle = "#FFFFFF"; // canvas background color
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    //Click en el ok del input
+    input.onchange = e => {
+
+        let file = e.target.files[0];
+
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+
+        reader.onload = readerEvent => {
+            let content = readerEvent.target.result;
+            let image = new Image();
+
+            image.src = content;
+
+            image.onload = function () {
+                img = this;
+                escalaFoto = Math.min(canvas.width / img.width, canvas.height / img.height);
+                ctx.drawImage(this, 0, 0, this.width * escalaFoto, this.height * escalaFoto);
+                ctx.lineCap = 'round';
+                cambiarColor();
+                rangoPincel();
+            }
+        }
+        input.value = "";
+    }
+
+    //FUNCION SIN EFECTO
+    let btnLimpio = document.querySelector('#btn-sinEfecto');
+    function sinEfecto() {
+        ctx.drawImage(img, 0, 0, img.width * escalaFoto, img.height * escalaFoto);
+    }
+    btnLimpio.addEventListener('click', sinEfecto);
+
+
+    //FUNCION GRISES
+    let btnGrises = document.querySelector('#btn-grises');
+    function efectoGris() {
+        let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
+        let pixels = imgData.data;
+        for (let i = 0; i < pixels.length; i += 4) {
+            let sum = pixels[i] + pixels[i + 1] + pixels[i + 2];
+            let avg = parseInt(sum / 3);
+            pixels[i] = avg;
+            pixels[i + 1] = avg;
+            pixels[i + 2] = avg;
+        }
+        imgData.data = pixels;
+        ctx.putImageData(imgData, 0, 0);
+    }
+    btnGrises.addEventListener('click', efectoGris);
+
+    //FUNCION SEPIA
+    let btnSepia = document.querySelector('#btn-sepia');
+    function efectoSepia() {
+        let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
+        let pixels = imgData.data;
+        for (let i = 0; i < pixels.length; i += 4) {
+            //calcula la luminosidad percibida para este pixel
+            let luminosidad = .3 * pixels[i] + .6 * pixels[i + 1] + .1 * pixels[i + 2];
+            pixels[i] = Math.min(luminosidad + 40, 255); // rojo
+            pixels[i + 1] = Math.min(luminosidad + 15, 255); // verde	
+            pixels[i + 2] = luminosidad; // azul																	
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    btnSepia.addEventListener('click', efectoSepia);
+
+    //FUNCION BINARIZACION
+    let btnBinarizacion = document.querySelector('#btn-binarizacion');
+    function efectoBinario() {
+        let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
+        let pixels = imgData.data;
+        for (let i = 0; i < pixels.length; i += 4) {
+            let color = (pixels[i] + pixels[i + 1] + pixels[i + 2]) / 3;
+            if (color > 126) {
+                pixels[i] = 255;
+                pixels[i + 1] = 255;
+                pixels[i + 2] = 255;
+            }
+            else {
+                pixels[i] = 0;
+                pixels[i + 1] = 0;
+                pixels[i + 2] = 0;
+            }
+
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    btnBinarizacion.addEventListener('click', efectoBinario);
+
+    //FUNCION BRILLO
+    let rangoBrillo = document.querySelector('#brillo');
+    let btnBrillo = document.querySelector('#btn-brillo');
+    function brillo() {
+        let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
+        let pixels = imgData.data;
+        let valor = parseInt(rangoBrillo.value, 10);
+        let constraste = valor;
+        console.log(constraste);
+        let factor = (259 * (constraste + 255)) / (255 * (259 - constraste));
+        for (let i = 0; i < pixels.length; i++) {
+            let r = pixels[i * 4];
+            let g = pixels[i * 4 + 1];
+            let b = pixels[i * 4 + 2];
+
+            pixels[i * 4] = factor * (r - 128) + 128;
+            pixels[i * 4 + 1] = factor * (g - 128) + 128;
+            pixels[i * 4 + 2] = factor * (b - 128) + 128;
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    btnBrillo.addEventListener('click', brillo);
+
+    //FUNCION NEGATIVO
+    let btnNegativo = document.querySelector('#btn-negativo');
+    function efectoNegativo() {
+        let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
+        let pixels = imgData.data;
+        for (let i = 0; i < pixels.length; i += 4) {
+            pixels[i] = 255 - pixels[i]; // R
+            pixels[i + 1] = 255 - pixels[i + 1]; // G
+            pixels[i + 2] = 255 - pixels[i + 2]; // B
+
+        }
+        ctx.putImageData(imgData, 0, 0);
+    }
+    btnNegativo.addEventListener('click', efectoNegativo);
+
+
+    saveButton.addEventListener('click', guardar);
+    btnLimpiar.addEventListener('click', limpiar);
+    lineWidthRange.addEventListener('input', rangoPincel);
+    colorPicker.addEventListener('change', cambiarColor);
+    document.querySelector('#borrar').addEventListener('click', cambiarColor);
+    document.querySelector('#dibujar').addEventListener('click', cambiarColor);
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mousemove', drawLine);
+    canvas.addEventListener('mouseup', stopDrawing);
+    canvas.addEventListener('mouseout', stopDrawing);
+
+}
+document.addEventListener("DOMContentLoaded", loadPage);
