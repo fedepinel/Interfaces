7c3c7291296bd99bde9875b65995d5549f340471
unknown <fede120400@gmail.com>
2020-09-13 18:18:46 -0300

    version final

--- Entregable1/js/javascript.js
+++ Entregable1/js/javascript.js
@@ -109,12 +109,12 @@ function loadPage() {
             let file = e.target.files[0];
             let reader = new FileReader();
             reader.readAsDataURL(file);
-    
+
             reader.onload = readerEvent => {
                 let content = readerEvent.target.result;
                 let image = new Image();
                 image.src = content;
-    
+
                 image.onload = function () {
                     img = this;
                     escalaFoto = Math.min(canvas.width / img.width, canvas.height / img.height);
@@ -126,7 +126,7 @@ function loadPage() {
                 limpiar();
             }
             input.value = "";
-        }      
+        }
     }
     document.querySelector('#btn-cargar').addEventListener("click", cargarFoto);
 
@@ -213,11 +213,70 @@ function loadPage() {
 
     //FUNCION BLUR
     let btnBlur = document.querySelector('#btn-blur');
+    let rangoBlur = document.querySelector('#blur');
     function efectoBlur() {
+        sinEfecto();
         let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
         let pixels = imgData.data;
+        let matrizGaussiana = [[1, 2, 1],
+        [2, 4, 2],
+        [1, 2, 1]];
+        let w = imgData.width;
+        let h = imgData.height;
+        let valorRangoBlur = parseInt(rangoBlur.value, 10);
+        for (let i = 0; i < valorRangoBlur; i++) {
+            for (let x = 0; x < w; x++) {
+                for (let y = 0; y < h; y++) {
+                    let ul = ((x - 1 + w) % w + w * ((y - 1 + h) % h)) * 4; // Posicion arriba-izquierda
+                    let uc = ((x - 0 + w) % w + w * ((y - 1 + h) % h)) * 4; // Posicion arriba-centro
+                    let ur = ((x + 1 + w) % w + w * ((y - 1 + h) % h)) * 4; // Posicion arriba-derecha
+                    let ml = ((x - 1 + w) % w + w * ((y + 0 + h) % h)) * 4; // Posicion izquierda
+                    let mc = ((x - 0 + w) % w + w * ((y + 0 + h) % h)) * 4; // Posicion pixel-central
+                    let mr = ((x + 1 + w) % w + w * ((y + 0 + h) % h)) * 4; // Posicion derecha
+                    let ll = ((x - 1 + w) % w + w * ((y + 1 + h) % h)) * 4; // Posicion abajo-izquierda
+                    let lc = ((x - 0 + w) % w + w * ((y + 1 + h) % h)) * 4; // Posicion abajo-central
+                    let lr = ((x + 1 + w) % w + w * ((y + 1 + h) % h)) * 4; // Posicion abajo-derecha
 
+                    p0 = pixels[ul] * matrizGaussiana[0][0]; // arriba-izquierda
+                    p1 = pixels[uc] * matrizGaussiana[0][1]; // arriba-centro
+                    p2 = pixels[ur] * matrizGaussiana[0][2]; // arriba-derecha
+                    p3 = pixels[ml] * matrizGaussiana[1][0]; // izquierda
+                    p4 = pixels[mc] * matrizGaussiana[1][1]; // pixel central
+                    p5 = pixels[mr] * matrizGaussiana[1][2]; // derecha
+                    p6 = pixels[ll] * matrizGaussiana[2][0]; // abajo-izquierda
+                    p7 = pixels[lc] * matrizGaussiana[2][1]; // abajo-central
+                    p8 = pixels[lr] * matrizGaussiana[2][2]; // abajo-derecha
+                    let red = (p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 16;
 
+                    p0 = pixels[ul + 1] * matrizGaussiana[0][0]; // arriba-izquierda
+                    p1 = pixels[uc + 1] * matrizGaussiana[0][1]; // arriba-centro
+                    p2 = pixels[ur + 1] * matrizGaussiana[0][2]; // arriba-derecha
+                    p3 = pixels[ml + 1] * matrizGaussiana[1][0]; // izquierda
+                    p4 = pixels[mc + 1] * matrizGaussiana[1][1]; // pixel central
+                    p5 = pixels[mr + 1] * matrizGaussiana[1][2]; // derecha
+                    p6 = pixels[ll + 1] * matrizGaussiana[2][0]; // abajo-izquierda
+                    p7 = pixels[lc + 1] * matrizGaussiana[2][1]; // abajo-central
+                    p8 = pixels[lr + 1] * matrizGaussiana[2][2]; // abajo-derecha
+                    let green = (p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 16;
+
+                    p0 = pixels[ul + 2] * matrizGaussiana[0][0]; // arriba-izquierda
+                    p1 = pixels[uc + 2] * matrizGaussiana[0][1]; // arriba-centro
+                    p2 = pixels[ur + 2] * matrizGaussiana[0][2]; // arriba-derecha
+                    p3 = pixels[ml + 2] * matrizGaussiana[1][0]; // izquierda
+                    p4 = pixels[mc + 2] * matrizGaussiana[1][1]; // pixel central
+                    p5 = pixels[mr + 2] * matrizGaussiana[1][2]; // derecha
+                    p6 = pixels[ll + 2] * matrizGaussiana[2][0]; // abajo-izquierda
+                    p7 = pixels[lc + 2] * matrizGaussiana[2][1]; // abajo-central
+                    p8 = pixels[lr + 2] * matrizGaussiana[2][2]; // abajo-derecha
+                    let blue = (p0 + p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8) / 16;
+
+                    pixels[mc] = red;
+                    pixels[mc + 1] = green;
+                    pixels[mc + 2] = blue;
+                    pixels[mc + 3] = pixels[lc + 3];
+                }
+            }
+        }
         ctx.putImageData(imgData, 0, 0);
     }
     btnBlur.addEventListener('click', efectoBlur);
@@ -229,18 +288,11 @@ function loadPage() {
         sinEfecto();
         let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
         let pixels = imgData.data;
-        let valor = parseInt(rangoBrillo.value, 10);
-        let constraste = valor;
-        console.log(constraste);
-        let factor = (259 * (constraste + 255)) / (255 * (259 - constraste));
+        let valor = rangoBrillo.value;
         for (let i = 0; i < pixels.length; i++) {
-            let r = pixels[i * 4];
-            let g = pixels[i * 4 + 1];
-            let b = pixels[i * 4 + 2];
-
-            pixels[i * 4] = factor * (r - 128) + 128;
-            pixels[i * 4 + 1] = factor * (g - 128) + 128;
-            pixels[i * 4 + 2] = factor * (b - 128) + 128;
+            pixels[i * 4] = pixels[i * 4] * valor;
+            pixels[i * 4 + 1] = pixels[i * 4 + 1] * valor;
+            pixels[i * 4 + 2] = pixels[i * 4 + 2] * valor;
         }
         ctx.putImageData(imgData, 0, 0);
     }
@@ -253,26 +305,103 @@ function loadPage() {
         sinEfecto();
         let imgData = ctx.getImageData(0, 0, img.width * escalaFoto, img.height * escalaFoto);
         let pixels = imgData.data;
+        let value = parseInt(rangoSaturacion.value, 10);
         for (let i = 0; i < pixels.length; i += 4) {
-            let value = parseInt(rangoSaturacion.value, 10);
-            let r = pixels[i];
-            let g = pixels[i + 1];
-            let b = pixels[i + 2];
-            let gray = 0.2989 * r + 0.5870 * g + 0.1140 * b; //weights from CCIR 601 spec
-            pixels[i] = -gray * value + pixels[i] * (1 + value);
-            pixels[i + 1] = -gray * value + pixels[i + 1] * (1 + value);
-            pixels[i + 2] = -gray * value + pixels[i + 2] * (1 + value);
-            //normalize over- and under-saturated values
-            if (pixels[i] > 255) pixels[i] = 255;
-            if (pixels[i + 1] > 255) pixels[i] = 255;
-            if (pixels[i + 2] > 255) pixels[i] = 255;
-            if (pixels[i] < 0) pixels[i] = 0;
-            if (pixels[i + 1] < 0) pixels[i] = 0;
-            if (pixels[i + 2] < 0) pixels[i] = 0;
+            let colorRgb = [pixels[i + 0], pixels[i + 1], pixels[i + 2]]
+            let colorHsv = RGBtoHSV(colorRgb);
+            colorHsv[1] *= value;
+            let colorfinal = HSVtoRGB(colorHsv);
+            pixels[i + 0] = colorfinal[0];
+            pixels[i + 1] = colorfinal[1];
+            pixels[i + 2] = colorfinal[2];
         }
         ctx.putImageData(imgData, 0, 0);
     }
     btnSaturacion.addEventListener('click', efectoSaturado);
+
+    RGBtoHSV = function (color) {
+        var r, g, b, h, s, v;
+        r = color[0];
+        g = color[1];
+        b = color[2];
+        min = Math.min(r, g, b);
+        max = Math.max(r, g, b);
+
+        v = max;
+        delta = max - min;
+        if (max != 0)
+            s = delta / max;        // s
+        else {
+            // r = g = b = 0        // s = 0, v is undefined
+            s = 0;
+            h = -1;
+            return [h, s, undefined];
+        }
+        if (r === max)
+            h = (g - b) / delta;      // between yellow & magenta
+        else if (g === max)
+            h = 2 + (b - r) / delta;  // between cyan & yellow
+        else
+            h = 4 + (r - g) / delta;  // between magenta & cyan
+        h *= 60;                // degrees
+        if (h < 0)
+            h += 360;
+        if (isNaN(h))
+            h = 0;
+        return [h, s, v];
+    };
+
+    HSVtoRGB = function (color) {
+        var i;
+        var h, s, v, r, g, b;
+        h = color[0];
+        s = color[1];
+        v = color[2];
+        if (s === 0) {
+            // achromatic (grey)
+            r = g = b = v;
+            return [r, g, b];
+        }
+        h /= 60;   
+        i = Math.floor(h);
+        f = h - i;
+        p = v * (1 - s);
+        q = v * (1 - s * f);
+        t = v * (1 - s * (1 - f));
+        switch (i) {
+            case 0:
+                r = v;
+                g = t;
+                b = p;
+                break;
+            case 1:
+                r = q;
+                g = v;
+                b = p;
+                break;
+            case 2:
+                r = p;
+                g = v;
+                b = t;
+                break;
+            case 3:
+                r = p;
+                g = q;
+                b = v;
+                break;
+            case 4:
+                r = t;
+                g = p;
+                b = v;
+                break;
+            default:        // case 5:
+                r = v;
+                g = p;
+                b = q;
+                break;
+        }
+        return [r, g, b];
+    }
 
     saveButton.addEventListener('click', guardar);
     btnLimpiar.addEventListener('click', limpiar);
