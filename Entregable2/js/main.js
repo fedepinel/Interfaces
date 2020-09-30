24fc18282dfc5c8325a0085467df30a425f19e8c
unknown <fede120400@gmail.com>
2020-09-29 23:19:04 -0300

    final

--- Entregable2/js/main.js
+++ Entregable2/js/main.js
@@ -1,10 +1,12 @@
 function loadPage() {
     let canvas = document.getElementById("canvas");
     var ctx = canvas.getContext('2d');
-    let boardW, boardH, boardArray, squareSize, color, canvasSize, boardTopMargin, playerTurn, countTurn, endGame, inRow, img, escalaFoto;
+    let boardW, boardH, boardArray, squareSize, color, canvasSize, playerTurn, countTurn, endGame, jugando, imgDrag;
+
     let tablero = 'imagenes/tablero.png';
     let fichaAmarilla = 'imagenes/fichaAmarilla.png';
     let fichaRoja = 'imagenes/fichaRoja.png';
+
 
 
     //Inicio de juego
@@ -17,7 +19,7 @@ function loadPage() {
 
     //Configuarcion general de datos
     function setUp() {
-        canvasSize = 800;
+        canvasSize = 700;
         inRow = 4;
         boardW = 7;
         boardH = 6;
@@ -28,12 +30,14 @@ function loadPage() {
                 2: "red"
             }
         };
+        jugando = false
         playerTurn = 1;
         countTurn = 0;
         endGame = false;
         setUpTablero();
         setUpCanvas();
         topText();
+
     }
 
     //Configura el tablero y la matriz
@@ -42,9 +46,13 @@ function loadPage() {
         image.src = tablero;
         image.onload = function () {
             img = this;
-            ctx.drawImage(this, 0, 0 + squareSize, boardW * squareSize, boardH * squareSize);
+            ctx.drawImage(this, 100, 0 + squareSize, boardW * squareSize, boardH * squareSize);
             draw();
+
+
         }
+
+
         boardArray = [];
         for (y = 0; y < boardH; y++) {
             let row = [];
@@ -53,6 +61,7 @@ function loadPage() {
             }
             boardArray.push(row);
         }
+
     }
 
     //Configura el canvas
@@ -62,12 +71,12 @@ function loadPage() {
             squareSize = canvasSize / (boardH + 1);
             canvas.width = boardW * squareSize;
         } else {
-            canvas.width = canvasSize;
-            console.log(canvas.width);
+            canvas.width = canvasSize + 200;
+
             squareSize = canvasSize / boardW;
             canvas.height = (boardH + 1) * squareSize;
-            console.log(canvas.height);
         }
+
     }
 
     //Dibuja las fichas en la matriz el valor 0 es el fondo blanco
@@ -77,40 +86,110 @@ function loadPage() {
                 drawTile(x, y + 1, boardArray[y][x]);
             }
         }
+        let imageFicha = new Image();
+        imageFicha.src = fichaAmarilla;
+        imageFicha.height = 75;
+        imageFicha.width = 75;
+        imageFicha.onload = function () {
+
+            ctx.drawImage(this, 0, 625, this.width, this.height);
+
+            imageFicha.src = fichaRoja;
+            imageFicha.onload = function () {
+
+                ctx.drawImage(this, 825, 625, this.width, this.height);
+                imgDrag = ctx.getImageData(0, 0, 900, 700);
+            }
+
+        }
     }
 
     //Dibuja la ficha
     function drawTile(x, y, tileColor) {
+        let img = new Image();
+        img.height = 75;
+        img.width = 75;
         let centerX = (x * squareSize) + (squareSize / 2);
         let centerY = (y * squareSize) + (squareSize / 2);
-        let tileSize = (squareSize * 0.8) / 2;
 
-        ctx.fillStyle = color.player[tileColor];
-        ctx.beginPath();
-        ctx.arc(centerX, centerY, tileSize, 0, 2 * Math.PI);
-        ctx.fill();
+        if ((jugando) && tileColor != 0) {
+            if (tileColor == 1) {
+                img.src = fichaAmarilla;
+            } else if (tileColor == 2) {
+                img.src = fichaRoja;
+            }
+            img.onload = function () {
+                ctx.drawImage(this, centerX + 62, centerY - 37, this.width, this.height);
+            }
+        }
+        if (tileColor == 0) {
+
+            let tileSize = (squareSize * 0.8) / 2;
+            ctx.fillStyle = color.player[tileColor];
+            ctx.beginPath();
+            ctx.arc(centerX + 100, centerY, tileSize, 0, 2 * Math.PI);
+            ctx.fill();
+        }
+
+
+
     }
 
     //Controla los movimientos del usuario
     function eventosJugador() {
+
         canvas.addEventListener("mousemove", (e) => {
-            let posX = Math.floor((e.clientX - canvas.offsetLeft) / squareSize);
-            if (!endGame) {
-                clearTopRow();
-                topText();
-                drawTile(posX, 0, playerTurn);
-            }
-        })
-        canvas.addEventListener("click", (e) => {
-            let clickX = Math.floor((e.clientX - canvas.offsetLeft) / squareSize)
-            if (!endGame) {
-                for (y = boardH - 1; y >= 0; y--) {
-                    if (boardArray[y][clickX] == 0) {
-                        playMove(clickX, y);
-                        break;
-                    }
+            if (jugando) {
+                let imgFicha = new Image();
+                imgFicha.height = 75;
+                imgFicha.width = 75;
+                if (playerTurn == 1) {
+                    imgFicha.src = fichaAmarilla;
+                } else if (playerTurn == 2) {
+                    imgFicha.src = fichaRoja;
+                }
+                let x = e.offsetX - 36;
+                let y = e.offsetY - 36;
+
+
+                ctx.putImageData(imgDrag, 0, 0);
+                imgFicha.onload = function () {
+                    ctx.drawImage(this, x, y, this.width, this.height);
                 }
             }
+
+
+        })
+
+        canvas.addEventListener("mousedown", (e) => {
+            let x = e.offsetX;
+            let y = e.offsetY;
+            if ((playerTurn == 1) && (x >= 0) && (x <= 75) && (y >= 625) && (y <= 700)) {
+                jugando = true;
+
+            } else if ((playerTurn == 2) && (x >= 825) && (x <= 900) && (y >= 625) && (y <= 700)) {
+                jugando = true;
+
+            }
+        })
+
+        canvas.addEventListener("mouseup", (e) => {
+            ctx.putImageData(imgDrag, 0, 0);
+            if ((jugando) && (e.offsetX > 100) && (e.offsetX < 798) && (e.offsetY > 100) && (e.offsetY < 198)) {
+                let clickX = Math.floor((e.clientX - (canvas.offsetLeft + 100)) / squareSize)
+
+                if (!endGame) {
+                    for (y = boardH - 1; y >= 0; y--) {
+                        if (boardArray[y][clickX] == 0) {
+                            playMove(clickX, y);
+                            break;
+                        }
+                    }
+                }
+
+            }
+            jugando = false;
+
         })
     }
 
@@ -125,8 +204,6 @@ function loadPage() {
         } else {
             cambiarJugador();
             clearTopRow();
-            topText("start");
-            drawTile(x, 0, playerTurn);
         }
         draw();
     }
@@ -202,5 +279,8 @@ function loadPage() {
         titulo.innerHTML = '4 en linea';
     }
     btnReset.addEventListener('click', reiniciar);
+
+
+
 }
 document.addEventListener("DOMContentLoaded", loadPage);
